6344a6d102d206a2198e74443953f353af118a4d
Ben Nuttall <benjaminlawrencedesigns@gmail.com>
2026-06-30 08:09:48 +0100

    Add sitemap integration and canonical site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://kentrendersystems.co.uk',
+  integrations: [sitemap()],
+});
